a8baf2895bbf94dad4288ec17905e904aecb2f97
TWoolff <92778181+TWoolff@users.noreply.github.com>
2021-11-22 12:12:37 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <input type="text" v-model="search">
+    <div v-for="project in filteredProjects" :key="project.id">
+      <Project :project="project" />
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Project from '@/components/Project'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Project },
+  data () {
+    return {
+      search: '',
+      projects: [
+        {
+          id: 1,
+          title: "Project 1",
+          body: "This is project 1"
+        },
+        {
+          id: 2,
+          title: "Project 2",
+          body: "This is project 2"
+        },
+        {
+          id: 3,
+          title: "Project 3",
+          body: "This is project 3"
+        }
+      ]
+    }
+  },
+  computed: {
+    filteredProjects() {
+      return this.projects.filter(project =>
+        project.body.toLowerCase().includes(this.search.toLowerCase())
+      );
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Project.vue
@@ -0,0 +1,12 @@
+<template>
+  <article>
+    <h2>{{ project.title }}</h2>
+    <p>{{ project.body }}</p>
+  </article>
+</template>
+
+<script>
+export default {
+  props: [ 'project' ]
+}
+</script>
